c12362f38e866f44efd146ffa49644f00cb3f28d
Jordan Buckley <149504654+JordanBuckleyGit@users.noreply.github.com>
2024-10-21 16:26:41 +0100

    Update reader.py

--- rando/reader.py
+++ rando/reader.py
@@ -12,3 +12,18 @@ def q1():
 
     inFile.close()
 q1()
+
+def q2():
+    inFile = open("lab5/debanks.txt", "r")
+
+
+    content = inFile.read()
+    lines = content.split("\n")
+
+    for line_number, line in enumerate(lines, start=1):
+        print(f"Line {line_number}: {line}")
+
+    inFile.close()
+q2()
+
+
