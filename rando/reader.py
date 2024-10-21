6d72f18c16e600af1da67ab84bb81a70f7eadde3
Jordan Buckley <149504654+JordanBuckleyGit@users.noreply.github.com>
2024-10-21 16:26:04 +0100

    Update reader.py

--- rando/reader.py
+++ rando/reader.py
@@ -1,1 +1,14 @@
 
+def q1():
+    inFile = open("lab5/debanks.txt", "r")
+
+    line_number = 1
+    line = inFile.readline()
+
+    while line:
+        print(f"Line {line_number}: {line.strip()}")
+        line_number += 1
+        line = inFile.readline()
+
+    inFile.close()
+q1()
